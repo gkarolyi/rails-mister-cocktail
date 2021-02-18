c7d1be032c15249f530ededeb4722c302558a361
Gergely <gergelykarolyi@gmail.com>
2021-02-18 11:41:22 +0100

    seeded ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url_path = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredients = JSON.parse(URI(url_path).read)
+
+ingredients['drinks'].each do |hash|
+  Ingredient.create(
+    name: hash['strIngredient1']
+  )
+end
